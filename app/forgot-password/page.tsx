fa72dab9fb7d1fd6cfa612ef88e6f0d34f9a0047
Zeek <ezekielabolade@gmail.com>
2025-01-09 06:56:11 +0100

    fixing bugs

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -6,7 +6,7 @@ import ResetForm from "@/components/forgot-password/reset-form";
 import ResetPassword from "@/components/forgot-password/reset-password";
 import React, { useState } from "react";
 
-export default function page() {
+export default function Page() {
   const [step, setStep] = useState(1);
   return (
     <div className="flex py-8 md:py-12 lg:py-20 px-4 md:px-8 lg:px-12 w-full justify-center items-center">
